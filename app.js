ff22749e8247257f65ffaad5167cb9216a13eff3
migblackmore@gmail.com <migblackmore@gmail.com>
2021-04-27 08:51:42 -0300

    Update Module 5

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
+const rootDir = require('./util/path')
+const path = require('path')
 const http = require('http');
-
+const bodyParser = require('body-parser')
 const express = require('express')
+
+// routes
+const adminRoutes = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
 
 const app = express()
 
-app.use('/add-product',(req, res, next)=>{
-    console.log('add product page!')
-    res.send('<h1>Add Product</h1><br><h3>Add</h3>')
-})
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(shopRoutes)
+app.use('/admin',adminRoutes)
 
-app.use('/',(req, res, next)=>{
-    console.log('root page')
-    res.send('<h1>Root</h1>')
+app.use((req,res,next)=>{
+    res.status(404).sendFile(path.join(rootDir,'views','404.html'))
 })
 
 app.listen(3000);
